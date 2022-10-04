ea0946ff5cf2dee391c39d491c3a833d9c37a2f4
Vasiliy7277 <klimovvv@mail.ru>
2022-10-04 21:01:01 +0300

    decided bug: /usr/bin/env: ‘node\r’

--- src/random.js
+++ src/random.js
@@ -1,5 +1,5 @@
 function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
-  export default getRandom;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export default getRandom;
